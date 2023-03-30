eeeff3090ee784e49c5d91c40b5c83b6c210819d
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-03-30 15:25:04 +0900

    アプリ全体：　基盤の作成
    ・WordListView：　ファイル作成
    ・WordListViewController：　ファイル作成
    ・WordModel：　ファイル作成と初期化

---
+++ WordApp/Controller/WordListViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class WordListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ WordApp/Model/Word.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Word {
+    var id: Int
+    var singleWord: String
+    var meaning: String
+    var exampleSentence: String
+    var exampleTranslation: String
+    var isRemembered: Bool
+    var wrongCount: Int
+}
