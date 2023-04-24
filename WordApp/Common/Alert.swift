67033dbb00ca28b0db63b6848038c10d17841003
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-04-24 21:41:43 +0900

    Common: コメントの追加

--- WordApp/Common/Alert.swift
+++ WordApp/Common/Alert.swift
@@ -1,10 +1,9 @@
 import UIKit
 
+// MARK: Alert
 /// UIViewController拡張(アラート)
 public extension UIViewController {
-
-    // MARK: Public Methods
-
+    // アラートを表示するメソッド
     func showAlert(title: String, message: String, actions: [UIAlertAction]) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         actions.forEach { alert.addAction($0) }
