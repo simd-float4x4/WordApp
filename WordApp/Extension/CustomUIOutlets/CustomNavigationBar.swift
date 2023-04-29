d3598850f127b3f7b3eee8f58bea4ea4b218f761
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-04-29 21:04:31 +0900

    暗記リスト／クイズ／設定画面：　ノッチの有無によってナビゲーションバーの高さを動的に変更する実装
    ・navigationBarFrameSize／navigationItemHeight／navigationBarUILabelPropertiesをletからvarに変更
    ・decideNavigationSizeByNotch()メソッド内で端末のsafeAreaを取得
    ・特定の範囲未満であれば、ナビゲーションバーの高さを低くするように実装

---
+++ WordApp/Extension/CustomUIOutlets/CustomNavigationBar.swift
@@ -0,0 +1,8 @@
+//
+//  CustomNavigationBar.swift
+//  WordApp
+//
+//  Created by Shumpei Horiuchi on 2023/04/29.
+//
+
+import Foundation
--- WordApp/View/Class/QuizView.swift
+++ WordApp/View/Class/QuizView.swift
@@ -54,15 +54,14 @@ class QuizView: UIView {
     let themeModel = DesignThemeListModel.shared
     //　ナビゲーションバータイトル
     let navigationBarTitleString = NSLocalizedString("QuizViewTitleText", comment: "")
-    // ナビゲーションバー：フレームサイズ（注意：iPhone X以降の端末）
-    // TODO: iPhone 8、SEなどにも対応できるようにする
-    let navigationBarFrameSize = (x: 0, y: 0, height: 94)
+    // ナビゲーションバー：フレームサイズ
+    var navigationBarFrameSize = (x: 0, y: 0, height: 94)
     // ナビゲーションアイテム：高さ
-    let navigationItemHeight = (x: 0, y: 0, height: 50)
+    var navigationItemHeight = (x: 0, y: 0, height: 50)
     // ステータスバー：高さ
     let statusBarHeight = 44
     //　ナビゲーションUILabel
-    let navigationBarUILabelProperties = (x: 0, y: 50, fontSize: CGFloat(16.0))
+    var navigationBarUILabelProperties = (x: 0, y: 50, fontSize: CGFloat(16.0))
     
     override init(frame: CGRect){
         super.init(frame: frame)
@@ -83,10 +82,24 @@ class QuizView: UIView {
         }
     }
     
+    //　ノッチによってナビゲーションバーのサイズを決定する
+    func decideNavigationSizeByNotch() {
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        //safeAreaの値が44.0以上であれば、ノッチがあるので、x位置をずらします。
+        if(window?.safeAreaInsets.top ?? 0.0 <= 44.0){
+            navigationBarFrameSize.height = 60
+            navigationBarUILabelProperties.y = 16
+        }
+    }
+    
     // UIを初期化する
     func initializeUI(parentView: UIView) {
         // 保存したテーマを取得する
         fetchSavedThemeData()
+        //　ノッチによってナビゲーションバーのサイズを決定する
+        decideNavigationSizeByNotch()
         //　テーマのアクセントカラーを取得する
         getAccentColor()
         //　アクセントカラーをセットする
@@ -186,7 +199,7 @@ class QuizView: UIView {
     // navigationBarのセットアップ
     func setUpNavigationBar(parentView : UIView) -> UINavigationBar {
         //　ナビゲーションバーのタイトルを取得
-        let wordRememberListNavigationItem = UINavigationItem(title: navigationBarTitleString)
+        let wordRememberListNavigationItem = UINavigationItem(title: "")
         // ナビゲーションバーをframeに合わせる
         var navBar = UINavigationBar(frame: CGRect(
             x: navigationBarFrameSize.x,
